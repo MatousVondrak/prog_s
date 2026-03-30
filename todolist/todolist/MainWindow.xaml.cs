7bf27be2b1838eab590ba0543b9e1d8d894a9837
MatousVondrak <142414614+MatousVondrak@users.noreply.github.com>
2026-03-30 09:04:12 +0200

    Add files via upload

---
+++ todolist/todolist/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+
+namespace todolist
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+
+    public class TodoItem
+    {
+        public string Text { get; set; }
+        public DateTime? Deadline { get; set; }
+    }
+
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<TodoItem> Tasks { get; set; } = new ObservableCollection<TodoItem>();
+
+        private string _newTaskText;
+        public string NewTaskText
+        {
+            get => _newTaskText;
+            set { _newTaskText = value; OnPropertyChanged(); }
+        }
+
+        private DateTime? _newTaskDeadline = DateTime.Today;
+        public DateTime? NewTaskDeadline
+        {
+            get => _newTaskDeadline;
+            set { _newTaskDeadline = value; OnPropertyChanged(); }
+        }
+
+        private TodoItem _selectedTask;
+        public TodoItem SelectedTask
+        {
+            get => _selectedTask;
+            set { _selectedTask = value; OnPropertyChanged(); }
+        }
+
+        public ICommand AddTaskCommand { get; }
+        public ICommand RemoveTaskCommand { get; }
+
+        public MainViewModel()
+        {
+            AddTaskCommand = new RelayCommand(AddTask, CanAddTask);
+            RemoveTaskCommand = new RelayCommand(RemoveTask, CanRemoveTask);
+        }
+
+        private void AddTask(object obj)
+        {
+            Tasks.Add(new TodoItem { Text = NewTaskText, Deadline = NewTaskDeadline });
+            NewTaskText = "";
+        }
+
+        private bool CanAddTask(object obj) => !string.IsNullOrWhiteSpace(NewTaskText);
+
+        private void RemoveTask(object obj) => Tasks.Remove(SelectedTask);
+
+        private bool CanRemoveTask(object obj) => SelectedTask != null;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter) => _canExecute == null || _canExecute(parameter);
+        public void Execute(object parameter) => _execute(parameter);
+    }
+}
